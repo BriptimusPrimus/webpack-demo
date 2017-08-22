c7b232fd779ac3595963bf35ec587199993177b3
Roberto Brito <brituor@gmail.com>
2017-08-21 21:17:17 -0700

    Linting JavaScript

--- webpack.config.js
+++ webpack.config.js
@@ -3,80 +3,117 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build'),
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build'),
 };
 
 const commonConfig = {
-    // Entries have to resolve to files! They rely on Node
-    // convention by default so if a directory contains *index.js*,
-    // it resolves to that.
-    entry: {
-        app: PATHS.app,
-    },
-    output: {
-        path: PATHS.build,
-        filename: '[name].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack demo',
-        }),
-    ],
+	// Entries have to resolve to files! They rely on Node
+	// convention by default so if a directory contains *index.js*,
+	// it resolves to that.
+	entry: {
+		app: PATHS.app,
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[name].js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack demo',
+		}),
+	],
 };
 
 const productionConfig = () => commonConfig;
 
 const developmentConfig = () => {
-    const config = {
-        devServer: {
-            // Enable history API fallback so HTML5 History API based
-            // routing works. Good for complex setups.
-            historyApiFallback: true,
+	const config = {
+		devServer: {
+			// overlay: true is equivalent
+			overlay: {
+				errors: true,
+				warnings: true,
+			},
+			
+			// Enable history API fallback so HTML5 History API based
+			// routing works. Good for complex setups.
+			historyApiFallback: true,
 
-            // Display only errors to reduce the amount of output.
-            stats: 'errors-only',
+			// Display only errors to reduce the amount of output.
+			stats: 'errors-only',
 
-            watchOptions: {
-                // Delay the rebuild after the first change
-                aggregateTimeout: 300,
+			watchOptions: {
+				// Delay the rebuild after the first change
+				aggregateTimeout: 300,
 
-                // Poll using interval (in ms, accepts boolean too)
-                poll: 1000,
-            },
+				// Poll using interval (in ms, accepts boolean too)
+				poll: 1000,
+			},
 
-            // Parse host and port from env to allow customization.
-            //
-            // If you use Docker, Vagrant or Cloud9, set
-            // host: options.host || '0.0.0.0';
-            //
-            // 0.0.0.0 is available to all network devices
-            // unlike default `localhost`.
-            host: process.env.HOST, // Defaults to `localhost`
-            port: process.env.PORT, // Defaults to 8080
-        },
-        plugins: [
-            // Ignore node_modules so CPU usage with poll
-            // watching drops significantly
-            new webpack.WatchIgnorePlugin([
-                path.join(__dirname, 'node_modules')
-            ])
-        ]
-    };
+			// Parse host and port from env to allow customization.
+			//
+			// If you use Docker, Vagrant or Cloud9, set
+			// host: options.host || '0.0.0.0';
+			//
+			// 0.0.0.0 is available to all network devices
+			// unlike default `localhost`.
+			host: process.env.HOST, // Defaults to `localhost`
+			port: process.env.PORT, // Defaults to 8080
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					enforce: 'pre',
 
-    Object.assign(config.plugins, commonConfig.plugins);
+					loader: 'eslint-loader',
+					options: {
+						emitWarning: true,
+					},
+				},
+			],
+		},
+		plugins: [
+			// Ignore node_modules so CPU usage with poll
+			// watching drops significantly
+			new webpack.WatchIgnorePlugin([
+				path.join(__dirname, 'node_modules'),
+			]),
+			new webpack.LoaderOptionsPlugin({
+				options: {
+					eslint: {
+						// Fail only on errors
+						failOnWarning: false,
+						failOnError: true,
 
-    return Object.assign(
-        {},
-        commonConfig,
-        config
-    );
+						// Toggle autofix
+						fix: false,
+
+						// Output to Jenkins compatible XML
+						outputReport: {
+							filePath: 'checkstyle.xml',
+							formatter: require('eslint/lib/formatters/checkstyle'),
+						},
+					},
+				},
+			}),
+		],		
+	};
+
+	Object.assign(config.plugins, commonConfig.plugins);
+
+	return Object.assign(
+		{},
+		commonConfig,
+		config
+	);
 };
 
 module.exports = (env) => {
-    if (env === 'production') {
-        return productionConfig();
-    }
+	if (env === 'production') {
+		return productionConfig();
+	}
 
-    return developmentConfig();
-}
+	return developmentConfig();
+};
