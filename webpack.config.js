0bff9354c30d7fb8f9faecda15a97e188b529770
Roberto Brito <brituor@gmail.com>
2017-08-24 20:19:51 -0700

    Loading Images

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,12 @@ const productionConfig = merge([
 	parts.purifyCSS({
 		paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
 	}),
+	parts.loadImages({
+		options: {
+			limit: 15000,
+			name: '[name].[ext]',
+		},
+	}),
 ]);
 
 const developmentConfig = merge([
@@ -52,6 +58,7 @@ const developmentConfig = merge([
 		port: process.env.PORT,
 	}),
 	parts.loadCSS(),
+	parts.loadImages(),
 ]);
 
 const developmentConfigBK = () => {
