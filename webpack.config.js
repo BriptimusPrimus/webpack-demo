3f7dae7215709586ce84d3b0ae096152223de16e
Roberto Brito <brituor@gmail.com>
2017-08-27 00:39:40 -0700

    Tidying Up

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
@@ -43,6 +42,7 @@ const commonConfig = merge([
 ]);
 
 const productionConfig = merge([
+	parts.clean(PATHS.build),
 	parts.generateSourceMaps({ type: 'source-map' }),
 	parts.extractCSS({
 		use: ['css-loader', parts.autoprefix()],
@@ -67,6 +67,7 @@ const productionConfig = merge([
 			),
 		},
 	]),
+	parts.attachRevision(),
 ]);
 
 const developmentConfig = merge([
@@ -85,89 +86,6 @@ const developmentConfig = merge([
 	parts.loadCSS(),
 	parts.loadImages(),
 ]);
-
-const developmentConfigBK = () => {
-	const config = {
-		devServer: {
-			// overlay: true is equivalent
-			overlay: {
-				errors: true,
-				warnings: true,
-			},
-			
-			// Enable history API fallback so HTML5 History API based
-			// routing works. Good for complex setups.
-			historyApiFallback: true,
-
-			// Display only errors to reduce the amount of output.
-			stats: 'errors-only',
-
-			watchOptions: {
-				// Delay the rebuild after the first change
-				aggregateTimeout: 300,
-
-				// Poll using interval (in ms, accepts boolean too)
-				poll: 1000,
-			},
-
-			// Parse host and port from env to allow customization.
-			//
-			// If you use Docker, Vagrant or Cloud9, set
-			// host: options.host || '0.0.0.0';
-			//
-			// 0.0.0.0 is available to all network devices
-			// unlike default `localhost`.
-			host: process.env.HOST, // Defaults to `localhost`
-			port: process.env.PORT, // Defaults to 8080
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					enforce: 'pre',
-
-					loader: 'eslint-loader',
-					options: {
-						emitWarning: true,
-					},
-				},
-			],
-		},
-		plugins: [
-			// Ignore node_modules so CPU usage with poll
-			// watching drops significantly
-			new webpack.WatchIgnorePlugin([
-				path.join(__dirname, 'node_modules'),
-			]),
-			new webpack.LoaderOptionsPlugin({
-				options: {
-					eslint: {
-						// Fail only on errors
-						failOnWarning: false,
-						failOnError: true,
-
-						// Toggle autofix
-						fix: false,
-
-						// Output to Jenkins compatible XML
-						outputReport: {
-							filePath: 'checkstyle.xml',
-							formatter: require('eslint/lib/formatters/checkstyle'),
-						},
-					},
-				},
-			}),
-		],
-	};
-
-	Object.assign(config.plugins, commonConfig.plugins);
-
-	return Object.assign(
-		{},
-		commonConfig,
-		config
-	);
-};
 
 module.exports = (env) => {
 	if (env === 'production') {
