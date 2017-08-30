8f9f65cf736a4c286d3381df866364065ef38861
Roberto Brito <brituor@gmail.com>
2017-08-29 22:49:22 -0700

    Bundling Libraries

---
+++ webpack.lib.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+const HappyPack = require('happypack');
+
+const PATHS = {
+	lib: path.join(__dirname, 'lib'),
+	build: path.join(__dirname, 'dist'),
+};
+
+const commonConfig = merge([
+	{
+		entry: {
+			lib: PATHS.lib,
+		},
+		output: {
+			path: PATHS.build,
+			library: 'Demo',
+			libraryTarget: 'var', // Default
+		},
+		plugins: [
+			new HappyPack({
+				loaders: [
+					// Capture Babel loader
+					'babel-loader',
+				],
+			}),
+		],
+	},
+	parts.attachRevision(),
+	parts.generateSourceMaps({ type: 'source-map' }),
+	parts.loadJavaScript({ include: PATHS.lib }),1,
+]);
+
+const libraryConfig = merge([
+	commonConfig,
+	{
+		output: {
+			filename: '[name].js',
+		},
+	},
+	parts.clean(PATHS.build),
+	parts.lintJavaScript({ incllude: PATHS.lib }),
+]);
+
+const libraryMinConfig = merge([
+	commonConfig,
+	{
+		output: {
+			filename: '[name].min.js',
+		},
+	},
+	parts.minifyJavascript(),
+]);
+
+module.exports = [
+	libraryConfig,
+	libraryMinConfig,
+];
